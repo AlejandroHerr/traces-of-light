1f9e8d8b3ead46bacf236b84c536bbf58117155c
AlejandroHerr <ahernandezc@protonmail.com>
2018-09-29 22:08:17 +0200

    feat(boilerpalte): Create boilerplate

---
+++ src/components/layout.js
@@ -0,0 +1,42 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
+
+import './layout.css';
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Fragment>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <div>
+          {children}
+        </div>
+      </Fragment>
+    )}
+  />
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Traces of light</h1>
+  </Layout>
+);
+
+export default IndexPage;
