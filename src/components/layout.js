bde90d790641c1e5b31473252d915e68a4b49a52
AlejandroHerr <ahernandezc@protonmail.com>
2018-09-30 23:14:34 +0200

    feat(Layout): Remove global styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 
-import './layout.css';
-
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
