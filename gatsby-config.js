c110e8efc14a21a52fea7119508b7d80c684a6f9
AlejandroHerr <ahernandezc@protonmail.com>
2018-09-30 14:45:13 +0200

    feat(content): Add content to galleries

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,12 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    'gatsby-transformer-json',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'images',
-        path: path.join(__dirname, 'images'),
+        name: 'data',
+        path: path.join(__dirname, 'data'),
       },
     },
     'gatsby-plugin-sharp',
