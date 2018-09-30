27e157f8228a08e347e1934d0aba88142f81e709
AlejandroHerr <ahernandezc@protonmail.com>
2018-09-30 23:13:27 +0200

    feat(Footer): Add license

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types';
 import Pagination from './Pagination';
 
 import styles from './Footer.module.scss';
+import License from './License';
 
 const Footer = ({
   first, index, last, pageCount,
 }) => (
   <footer className={styles.footer}>
+    <div className={styles.footer_license}>
+      <License />
+    </div>
     <div className={styles.footer_pagination}>
       <Pagination first={first} index={index} last={last} pageCount={pageCount} />
     </div>
