dba6b4837dba1e8e139437354f2be5b5a62848b1
AlejandroHerr <ahernandezc@protonmail.com>
2018-10-08 23:36:38 +0200

    feat(manifest): Update icon and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,13 +17,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Traces of Light',
+        short_name: 'Traces of Light',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#ffffff',
+        theme_color: '#373737',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-transformer-json',
