460d5bf4dcc9489390327795119c0bceebb03fa8
AlejandroHerr <ahernandezc@protonmail.com>
2018-10-09 23:25:21 +0200

    feat(Header): Add link to home in title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import GatsbyLink from 'gatsby-link';
 import styles from './Header.module.scss';
 
 const Header = ({ title }) => (
   <header className={styles.header}>
-    <div className={styles.header_title}>
-      <h1>{title}</h1>
+    <div className={styles.header_title_rapper}>
+      <GatsbyLink className={styles.header_title_link} to="/">
+        <h1 className={styles.header_title}>{title}</h1>
+      </GatsbyLink>
     </div>
   </header>
 );
