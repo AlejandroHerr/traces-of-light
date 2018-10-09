151941c8a53cb6a6f3b6d6acd797eb3e9cc7c7d8
AlejandroHerr <ahernandezc@protonmail.com>
2018-10-10 00:07:30 +0200

    revert(SEO): Revert meta tags changes

--- src/components/Layout/LayoutRenderer.js
+++ src/components/Layout/LayoutRenderer.js
@@ -11,34 +11,30 @@ const buildUrl = (base, pathname = '') => (!pathname
 const LayoutRenderer = ({
   children, image, pathname, siteMetadata,
 }) => (
-  <>
-    <Helmet
-      title={siteMetadata.title}
-      meta={[
-        { name: 'description', content: siteMetadata.description },
-        { name: 'keywords', content: siteMetadata.keywords },
-        { name: 'twitter:card', content: 'summary' },
-        { name: 'twitter:title', content: siteMetadata.title },
-        { name: 'twitter:description', content: siteMetadata.description },
-        { name: 'twitter:image', content: image && image.src && buildUrl(siteMetadata.canonical, image.src) },
-      ]}
-    >
-      <html lang="en" />
-      <link rel="canonical" href={buildUrl(siteMetadata.canonical, pathname)} />
-      <meta property="og:url" content={buildUrl(siteMetadata.canonical, pathname)} />
-      <meta property="og:title" content={siteMetadata.title} />
-      <meta property="og:description" content={siteMetadata.description} />
-      <meta property="og:type" content="website" />
-      <meta
-        property="og:image"
-        content={image && image.src && buildUrl(siteMetadata.canonical, image.src)}
-      />
-    </Helmet>
-    <div>
-      <Header title={siteMetadata.title} />
-      {children}
-    </div>
-  </>
+    <>
+      <Helmet
+        title={siteMetadata.title}
+        meta={[
+          { name: 'description', content: siteMetadata.description },
+          { name: 'keywords', content: siteMetadata.keywords },
+          { name: 'og:url', content: buildUrl(siteMetadata.canonical, pathname) },
+          { name: 'og:title', content: siteMetadata.title },
+          { name: 'og:description', content: siteMetadata.description },
+          { name: 'og:image', content: image && image.src && buildUrl(siteMetadata.canonical, image.src) },
+          { name: 'og:type', content: 'website' },
+          { name: 'twitter:card', content: 'summary' },
+          { name: 'twitter:title', content: siteMetadata.title },
+          { name: 'twitter:description', content: siteMetadata.description },
+          { name: 'twitter:image', content: image && image.src && buildUrl(siteMetadata.canonical, image.src) },
+        ]}
+      >
+        <html lang="en" />
+      </Helmet>
+      <div>
+        <Header title={siteMetadata.title} />
+        {children}
+      </div>
+    </>
 );
 
 LayoutRenderer.propTypes = {
