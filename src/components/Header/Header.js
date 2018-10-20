ea471e7641babdb6ad52ddf6e5792b4c8134a8bd
AlejandroHerr <ahernandezc@protonmail.com>
2018-10-20 14:46:22 +0200

    fix(Header): Fix className

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,11 +6,11 @@ import styles from './Header.module.scss';
 
 const Header = ({ title }) => (
   <header className={styles.header}>
-    <div className={styles.header_title_rapper}>
+    <section className={styles.header_title_wrapper}>
       <GatsbyLink className={styles.header_title_link} to="/">
         <h1 className={styles.header_title}>{title}</h1>
       </GatsbyLink>
-    </div>
+    </section>
   </header>
 );
 
